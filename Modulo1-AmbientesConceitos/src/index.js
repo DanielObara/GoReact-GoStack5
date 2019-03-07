f9e34e1b071ee053f954e8cd96ea751ed0a09885
daniel.obara <daniel.tsutomu@hotmail.com>
2019-03-07 15:18:07 -0300

    Eslint + Prettier configurations

--- Modulo1-AmbientesConceitos/src/index.js
+++ Modulo1-AmbientesConceitos/src/index.js
@@ -1,15 +1,15 @@
-import React, { Component, Fragment } from "react";
-import { render } from "react-dom";
-import Button from "./Button";
-import "./style.scss";
+import React, { Component, Fragment } from 'react';
+import { render } from 'react-dom';
+import Button from './Button';
+import './style.scss';
 
 class App extends Component {
   state = {
-    counter: 0
+    counter: 0,
   };
 
-  //Executa na inicialização do componente
-  //Ideal para consulta API e EventListners
+  // Executa na inicialização do componente
+  // Ideal para consulta API e EventListners
   // componentDidMount() {}
 
   // //Contrario do DidMount, ideal para limpar os eventListners etc.
@@ -22,26 +22,26 @@ class App extends Component {
   // componentDidUpdate = (prevProps, prevState) => {};
 
   handleClick = () => {
-    //o SetState é assincrona então ele leva um tempo para ser efetuado
+    // o SetState é assincrona então ele leva um tempo para ser efetuado
     // this.setState({ counter: this.state.counter + 1 });
-    //se colocassemos o console.log aqui ele apresentaria ainda o 0 pois ainda não foi atualizado o estado.
+    // se colocassemos o console.log aqui ele apresentaria ainda o 0 pois ainda não foi atualizado o estado.
 
-    /* 
+    /*
       Desta forma o state passado como parametro é alterado automaticamente.
     */
     this.setState(state => ({ counter: this.state.counter + 1 }));
   };
 
   render() {
-    console.log("render");
+    console.log('render');
     return (
       <Fragment>
         <h1>Hello World</h1>
-        <h2 style={{ color: "#f00" }}>{this.state.counter}</h2>
+        <h2 style={{ color: '#f00' }}>{counter}</h2>
         <Button onClick={this.handleClick}>Somar</Button>
       </Fragment>
     );
   }
 }
 
-render(<App />, document.getElementById("app"));
+render(<App />, document.getElementById('app'));
