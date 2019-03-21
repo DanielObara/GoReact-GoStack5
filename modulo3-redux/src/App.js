0209493d2a19d6623f743e88e8812a51c898c8c3
Daniel Obara <daniel.tsutomu@hotmail.com>
2019-03-21 15:12:02 -0300

    Initialize module 3
    Add redux and create store

---
+++ modulo3-redux/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import store from "./store";
+
+const App = () => (
+	<Provider store={store}>
+		<h1>Teste</h1>
+	</Provider>
+);
+
+export default App;
