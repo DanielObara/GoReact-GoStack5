520278de2925af146b4d083f3c55c7b2652b2450
Daniel Obara <daniel.tsutomu@hotmail.com>
2019-03-22 21:36:15 -0300

    Config babel and Webpack

---
+++ Desafio-Modulo-1/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "react-dom";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
